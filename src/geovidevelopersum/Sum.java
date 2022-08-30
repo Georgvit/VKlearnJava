e8bb4bcb2efef22b5f309535d9f035f30978701a
Georgvit <ifreyd@mail.ru>
2022-08-30 11:54:23 +0300

    sum

---
+++ src/geovidevelopersum/Sum.java
@@ -0,0 +1,12 @@
+package geovidevelopersum;
+
+public class Sum {
+   public static void main(String[]args){
+       int x = 3;
+       int y = 4;
+       int z = 8;
+       int sum = x+ y + z;
+       System.out.println("x + y + z = " + sum );
+   }
+
+}
